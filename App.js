3c10dc3ae96ea709b36998922d32e25506adbc62
Diego Plaza <d.plazagarrido@gmail.com>
2020-02-20 22:26:27 -0300

    feat: add task

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Todo App!</Text>
-    </View>
-  );
+class App extends React.Component{
+  state = {
+    text: "",
+    todo: []
+  }
+  addTodo  = () => {
+    var newTodo = this.state.text
+    var arr = this.state.todo
+    arr.push(newTodo)
+    this.setState({todo: arr, text: ""})
+  }
+  renderTodos = () => {
+    return this.state.todo.map(t => {
+      return (
+        <Text>{ t }</Text>
+      )
+    })
+  }
+  render(){
+    return(
+      <View style={style.viewStyle}>
+        <Text>Todo App!</Text>
+        <TextInput
+          style={style.inputStyle}
+          onChangeText={(text) => this.setState({text})}
+          value = {this.state.text}
+        />
+        <Button
+          title='Add Todo'
+          color= 'green'
+          onPress={this.addTodo}
+        />
+        {this.renderTodos()}
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const style = {
+  viewStyle: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
   },
-});
+  inputStyle: {
+    height: 40,
+    borderColor: 'green',
+    borderWidth: 1
+  }
+}
+
+export default App 
